8b3e80a86496b2afacefe31c419cab496fc7e59c
Jakob Farian Krarup <kr@rup.dk>
2026-09-23 10:43:46 +0200

    Added class diagram

--- Program.cs
+++ Program.cs
@@ -3,16 +3,14 @@ using AsyncConversion.DataAccess;
 using AsyncConversion.UI;
 
 namespace AsyncConversion;
-
 internal class Program
 {
     static void Main(string[] args)
     {
-        DataAccess.DataAccessLayer dataAccess = new();
-        BusinessLogic.BusinessLogicLayer businessLogicLayer = new(dataAccess);
+        DataAccessLayer dataAccess = new();
+        BusinessLogicLayer businessLogicLayer = new(dataAccess);
         UILayer ui = new(businessLogicLayer);
 
         ui.Run();
-
     }
 }
